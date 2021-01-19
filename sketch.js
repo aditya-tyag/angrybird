ab30358313646d67bd7b32327901a66a3250f8f4
aditya-tyag <tfg.adesh@gmail.com>
2021-01-19 20:42:03 +0530

    added chain class

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
+
 var box1, pig1;
+var log6,chain;
+
 var backgroundImg,platform;
 
 function preload() {
@@ -33,17 +37,16 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6= new Log(230,180,80,PI/2);
 
     bird = new Bird(100,100);
 
+    chain= new Chain(bird.body,log6.body);
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -58,7 +61,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
     platform.display();
+    chain.display();   
 }
